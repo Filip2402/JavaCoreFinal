1ee4a421d107967616cf417ca6597fa7b466b939
holkwen <vlad.kryshtalov@gmx.com>
2017-01-19 21:21:21 +0200

    User / UserDAO update

--- src/project/User.java
+++ src/project/User.java
@@ -1,8 +1,23 @@
 package project;
 
-public class User {
+public class User{
     private long id;
     private String firstName;
     private String lastName;
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return email.hashCode();
+    }
 }
--- src/project/UserDAO.java
+++ src/project/UserDAO.java
@@ -1,9 +1,18 @@
 package project;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class UserDAO extends DAO<User> {
+    private Set<User> set = new HashSet<>();
+
+    @Override
+    public boolean add(User user) {
+        return set.add(user);
+    }
+
     @Override
     public boolean update(User user) {
         return false;
