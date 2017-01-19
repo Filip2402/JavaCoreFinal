8258be5082ceea009b84e45b8eeb5dfd60ffef89
holkwen <vlad.kryshtalov@gmx.com>
2017-01-19 16:16:16 +0200

    controller update

--- src/project/Controller.java
+++ src/project/Controller.java
@@ -1,4 +1,7 @@
 package project;
 
 public class Controller {
+    private static DAO<Hotel> hotelDAO = new HotelDAO();
+    private static DAO<Room> roomDAO = new RoomDAO();
+    private static DAO<User> userDAO = new UserDAO();
 }
